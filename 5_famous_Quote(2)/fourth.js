8aa3ffd4921603eabba48bd3cd1ce6b6a852de20
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 10:21:43 -0700

    45 Question of Typescript assignment

---
+++ 5_famous_Quote(2)/fourth.js
@@ -0,0 +1,5 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+//Then compose your message and store it in a new variable called message. Print your message.
+var famousPerson = "Hazrat Umar bin Khattab(R.A)";
+var message = "The best way to defeat someone is to beat him at politness";
+console.log("".concat(famousPerson, " said,\"").concat(message, "\""));
---
+++ 5_famous_Quote(2)/fourth.ts
@@ -0,0 +1,6 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+//Then compose your message and store it in a new variable called message. Print your message.
+let famousPerson:string = "Hazrat Umar bin Khattab(R.A)";
+let message:string = "The best way to defeat someone is to beat him at politness";
+console.log(`${famousPerson} said,"${message}"`);
+

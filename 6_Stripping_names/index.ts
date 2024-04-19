73ce69fa85926631ac51f0032332078ac31870b1
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 10:24:09 -0700

    45 Question of Typescript Assignment

---
+++ 6_Stripping_names/index.ts
@@ -0,0 +1,8 @@
+//QUESTION NO 6.Stripping Names: Store a person’s name, and include some whitespace characters at the beginning 
+//and end of the name Make sure you use each character combination, "\t" and "\n", at least once.
+// Print the name once, so the whitespace around the name is displayed. Then print the name after 
+//striping the white spaces.
+let personName:string="  \n\tZakheer Ali \t\n  ";
+console.log(personName);
+console.log(personName.trim());
+

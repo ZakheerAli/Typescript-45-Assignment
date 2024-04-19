a7fbb761a172710eda704e191a859a5ef225542d
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 10:19:47 -0700

    45 Question of Typescript assignment

---
+++ 4_Famous_Quote/third.ts
@@ -0,0 +1,4 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+//Your output should look something like the following, including the quotation marks:
+let author:string = "Hazrat Ali(R.A)";
+console.log(`${author} once said,"There are two ways to live a pleasant life,either someone's heart or in someone's prayer"`);

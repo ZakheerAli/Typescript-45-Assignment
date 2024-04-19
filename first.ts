8589266d4c6fc5fb24ba219420137141088ad940
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 10:04:12 -0700

    Add files via upload

---
+++ first.ts
@@ -0,0 +1,12 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person.
+//  Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let personName:string = "Zakheer Ali";
+console.log(`Hello ${personName},would you like to learn some python today?`);
+
+                               //OR
+
+let person1:string = "Zakheer Ali";
+console.log("Hello "+person1+",would you like to learn some python today?");
+
+

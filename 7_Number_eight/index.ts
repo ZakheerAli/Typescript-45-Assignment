a9971fb4999dace05a0609293c48e4e02973deb5
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 10:26:41 -0700

    45 Question of Typescript assignment

---
+++ 7_Number_eight/index.ts
@@ -0,0 +1,6 @@
+//QUESTION NO 7: Number Eight: Write addition, subtraction, multiplication, and division operations
+// that each result in the number 8.Be sure to enclose your operations in print statements to see the results.
+console.log("5+3=", 5 + 3 );//ADDITION
+console.log("10-2=", 10 - 2 );//SUBTRACTION
+console.log("2*4=", 2 * 4 );//MULTIPLICATION
+console.log("16/2=", 16 / 2 );//DIVISION

3d549d8cc7f3be7839ae7c28482f2931e5640593
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 10:30:23 -0700

    45 Question of Typescript assignment

---
+++ 9_Favorite_number/index.js
@@ -0,0 +1,5 @@
+//QUESTION NO 9:Favorite Number: Store your favorite number in a variable. Then, using that
+// variable, create a message that reveals your favorite number. Print that message.
+var favoriteNumber = 86;
+var message = "My favorite number is ".concat(favoriteNumber);
+console.log(message);
---
+++ 9_Favorite_number/index.ts
@@ -0,0 +1,5 @@
+//QUESTION NO 9:Favorite Number: Store your favorite number in a variable. Then, using that
+// variable, create a message that reveals your favorite number. Print that message.
+let favoriteNumber:number =86;
+let message:string=`My favorite number is ${favoriteNumber}`;
+console.log(message);

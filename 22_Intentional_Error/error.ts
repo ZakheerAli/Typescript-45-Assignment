b7ff3c09b400bec736654c96deb66003a5e56d75
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 18:35:16 -0700

    45 Question of Typescript assignment

---
+++ 22_Intentional_Error/error.ts
@@ -0,0 +1,7 @@
+//Question no 22:Intentional Error: If you haven’t received an array index error in one of your 
+//programs yet, try to make one happen. Change an index in one of your programs to produce an index error.
+// Make sure you correct the error before closing the program.
+
+let newArray:string[]=["car","cycle","aeroplane","train"];
+console.log(newArray[5]);
+console.log(newArray);

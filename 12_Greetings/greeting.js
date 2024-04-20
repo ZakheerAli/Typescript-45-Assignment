34dbbbd87330958121de31e98466e7f7536f087a
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 18:14:23 -0700

    45 Question of Typescript assignment

---
+++ 12_Greetings/greeting.js
@@ -0,0 +1,7 @@
+//Question no 12 :Greetings: Start with the array you used in Exercise 11, but instead of just printing each 
+//person’s name, print a message to them. The text of each message should be the same, but each 
+//message should be personalized with the person’s name.
+var names = ["Ali", "Zakheer", "zaheer", "Osman"];
+for (var i = 0; i < names.length; i++) {
+    console.log("here is your good name", names[i]);
+}
---
+++ 12_Greetings/greeting.ts
@@ -0,0 +1,9 @@
+//Question no 12 :Greetings: Start with the array you used in Exercise 11, but instead of just printing each 
+//person’s name, print a message to them. The text of each message should be the same, but each 
+//message should be personalized with the person’s name.
+
+let names:string[]=["Ali","Zakheer","zaheer","Osman"]
+for(let i=0; i<names.length; i++){
+    console.log(`here is your good name`,names[i]);
+}
+

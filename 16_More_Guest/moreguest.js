b791f2d93879fe886b450b41daae6bff2a6c3cc1
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 18:23:02 -0700

    45 Question of Typescript assignment

---
+++ 16_More_Guest/moreguest.js
@@ -0,0 +1,27 @@
+//Question no 16: More Guests: You just found a bigger dinner table, so now more space is available. 
+//Think of three more guests to invite to dinner.
+//• Start with your program from Exercise 15. Add a print statement to the end of
+// your program informing people that you found a bigger dinner table.
+//• Add one new guest to the beginning of your array.
+//• Add one new guest to the middle of your array. • Use append() to add one new guest to 
+//the end of your list. • Print a new set of invitation messages, one for each person in your list.
+//humne ex#15 se start ky hain
+var Guestlist = ["Billgates", "Elon musk", "Mark zukerberg"];
+for (var i = 0; i < Guestlist.length; i++) {
+    console.log("Dear Mr" + Guestlist[i] + "\nIt is our pleasure to invite you in our party\nThank you!\n\n");
+}
+var absentguest = "billgates";
+var newguest = "Imran khan";
+Guestlist[0] = newguest;
+for (var i = 0; i < Guestlist.length; i++) {
+    console.log("Dear Mr" + Guestlist[i] + "\nIt is our pleasure to invite you in our party\nThank you!\n\n");
+}
+console.log("Mr ".concat(absentguest, " will not come to the party"));
+//phir humne bare table ka message print kya hai
+console.log("Good news! we found a big dinner table so we are inviting 3 more guest");
+Guestlist.unshift("Nawaz Shareef");
+Guestlist.splice(2, 0, "Maryam Nawaz");
+Guestlist.push("Shehbaz shareef");
+for (var i = 0; i < Guestlist.length; i++) {
+    console.log("Dear Mr" + Guestlist[i] + "\nIt is our pleasure to invite you in our party\nThank you!\n\n");
+}

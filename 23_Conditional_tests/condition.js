0e945a0be31380be80879fbb10ca256edde8e128
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-19 18:37:44 -0700

    45 Question of typescript assignment

---
+++ 23_Conditional_tests/condition.js
@@ -0,0 +1,39 @@
+"use strict";
+//Conditional Tests: Write a series of conditional tests. Print a statement describing
+// each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+//TEST NO 1
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+//TEST NO 2
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+//TEST NO 3
+console.log("Is car != 'SUBARU'? I predict True.");
+console.log(car != 'SUBARU');
+//TEST NO 4
+console.log("Is car !== 'SUBARU'? I predict True.");
+console.log(car !== 'SUBARU');
+//TEST NO 5
+console.log("Is car != 'toyota'? I predict True.");
+console.log(car != 'toyota');
+//TEST NO 6
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota');
+//TEST NO 7
+console.log("Is car == 'SUBARU'? I predict False.");
+console.log(car == 'SUBARU');
+//TEST NO 8
+console.log("Is car ==='landcruiser'? I predict False.");
+console.log(car === "landcruiser");
+//TEST NO 9
+console.log("Is car !='subaru'? I predict False.");
+console.log(car != "subaru");
+//TEST NO 10
+console.log("Is car =='toyota'? I predict False.");
+console.log(car == "toyota");

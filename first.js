d71f9376af91aa400fcb1e31b56c485fa5fc87d1
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-20 22:29:53 -0700

    Delete first.js

--- first.js
+++
@@ -1,7 +0,0 @@
-// Personal Message: Store a person’s name in a variable, and print a message to that person.
-//  Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-var personName = "Zakheer Ali";
-console.log("Hello ".concat(personName, ",would you like to learn some python today?"));
-//OR
-var person1 = "Zakheer Ali";
-console.log("Hello " + person1 + ",would you like to learn some python today?");

2f531ab35d7b01de037bd4bcce3185aee9bf312e
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-20 22:16:07 -0700

    45 Question of Typescript assignment

---
+++ 37_Large_shirts/largeshirt.js
@@ -0,0 +1,10 @@
+//Question no 37:Large Shirts: Modify the make_shirt() function so that shirts are large
+// by default with a message that reads I love TypeScript. Make a large shirt and a medium 
+//shirt with the default message, and a shirt of any size with a different message.
+function make_shirt(size, text) {
+    if (text === void 0) { text = "I love typescript"; }
+    console.log("Creating a ".concat(size, " shirt with the message :").concat(text, " "));
+}
+make_shirt("large");
+make_shirt("medium");
+make_shirt("small", "hello world");

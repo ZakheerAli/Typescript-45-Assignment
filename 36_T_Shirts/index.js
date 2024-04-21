b444164170645a05b1b00776bbd8db56ccc04471
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-20 22:14:46 -0700

    45 Question of Typescript assignment

---
+++ 36_T_Shirts/index.js
@@ -0,0 +1,9 @@
+//Question no 36:T-Shirt: Write a function called make_shirt() that accepts a size and the
+// text of a message that should be printed on the shirt. The function should print a sentence 
+//summarizing the size of the shirt and the message printed on it. Call the function.
+function make_shirt(size, text) {
+    console.log("Creating a ".concat(size, " shirt with the message:").concat(text));
+}
+make_shirt("small", "my name is Zakheer Ali");
+make_shirt("medium", "my name is Zakheer Ali");
+make_shirt("large", "my name is Zakheer Ali");

cf0ab87402bb6944c43249f9c6a114ad58eeb2c5
ZakheerAli <159714713+ZakheerAli@users.noreply.github.com>
2024-04-20 22:03:28 -0700

    45 Question of Typescript assignment

---
+++ 31_Checking-usernames/index.js
@@ -0,0 +1,20 @@
+//question no 32: Checking Usernames: Do the following to create a program that simulates 
+//how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the 
+//new usernames are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used.
+// If it has, print a message that the person will need to enter a new username.
+// If a username has not been used, print a message saying that the username is available.
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+var current_users = ["TaHa", "DaNiYal", "saLmAn", "Hamzah", "john"];
+var new_users = ["ali", "TaHa", "dilsher", "jameel", "Hamzah"];
+new_users.forEach(function (newusername) {
+    var lowerCase = newusername.toLowerCase();
+    if (current_users.map(function (c_user) { return c_user.toLowerCase(); }).includes(lowerCase)) {
+        console.log("The username ".concat(newusername, " is not available.please write different."));
+    }
+    else {
+        console.log("The Username ".concat(newusername, " is availble"));
+    }
+});
